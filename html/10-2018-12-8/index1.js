c8927387ec96bbbbfec8f503ceba694e7f5340b2
Toan48 <dinhtuongtoan48@gmail.com>
2018-12-10 21:49:56 +0700

    update 2018-12-10

--- html/10-2018-12-8/index1.js
+++ html/10-2018-12-8/index1.js
@@ -1,13 +1,28 @@
-document.getElementById('btnon').addEventListener('click', changelight)
-function changelight()
-{
-    var image = document.getElementById("img1");
-    if (image.src.match("bulbon")) 
+// document.getElementById('btnon').addEventListener('click', changelight)
+// function changelight()
+// {
+//     var image = document.getElementById("img1");
+//     if (image.src.match("bulbon")) 
+//     {
+//         image.src = "./pic_bulboff.gif";
+//     } 
+//     else 
+//     {
+//         image.src = "./pic_bulbon.gif";
+//     }
+// }   
+
+$(doccument).ready(function(){
+    $("button").click(function()
     {
+        var image = document.getElementById("img1");
+  if (image.src.match("bulbon")) 
+     {
         image.src = "./pic_bulboff.gif";
-    } 
-    else 
-    {
-        image.src = "./pic_bulbon.gif";
-    }
-}   
+     } 
+     else 
+     {
+       image.src = "./pic_bulbon.gif";
+     }
+    })
+})
