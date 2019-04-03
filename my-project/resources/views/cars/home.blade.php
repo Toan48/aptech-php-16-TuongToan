fe4f9b7e88d9d95f9e9b3f62d78c357703acda51
Toan48 <dinhtuongtoan48@gmail.com>
2019-04-03 09:59:36 +0700

    updated

--- my-project/resources/views/cars/home.blade.php
+++ my-project/resources/views/cars/home.blade.php
@@ -18,10 +18,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     
-   
 
 
-   
+
+    
 </head>
 
 <body>
